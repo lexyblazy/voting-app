c473c25dc929d4507aa6dd7a4a430907b82f62dc
lexy <lexyblazy@gmail.com>
2018-04-10 14:59:42 +0100

    completed twitter oauth flow

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 //absolute imports
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const session = require("express-session");
+const passport = require("passport");
 const PORT = process.env.PORT || 5000;
+
+//tell Mongoose to use es6 promises
+mongoose.Promise = global.Promise;
 
 //relative imports
 const authRoutes = require("./routes/authRoutes");
 const { SECRET } = require("./config/keys");
+
+//connect to the database;
+const db = async () => {
+  await mongoose.connect("mongodb://localhost:27017/voting-app");
+  console.log("Connection the db was successful");
+};
+
+try {
+  db();
+} catch (error) {
+  console.log(error);
+}
 
 app.use(
   session({
@@ -15,6 +32,9 @@ app.use(
     saveUninitialized: false
   })
 );
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require("./services/passport");
 app.use(authRoutes);
--- services/passport.js
+++ services/passport.js
@@ -1,23 +1,45 @@
 const passport = require("passport");
 const TwitterStrategy = require("passport-twitter").Strategy;
+const User = require("../models/User");
 
 const {
   TWITTER_CONSUMER_KEY,
   TWITTER_CONSUMER_SECRET
 } = require("../config/keys");
 
-passport.use(new TwitterStrategy({
-  consumerKey: TWITTER_CONSUMER_KEY,
-  consumerSecret: TWITTER_CONSUMER_SECRET,
-  callbackURL: "/auth/twitter/callback"
-},
-function(token, tokenSecret, profile, cb) {
-  // User.findOrCreate({ twitterId: profile.id }, function (err, user) {
-  //   return cb(err, user);
-  // });
-  console.log("Profile =>",profile);
-  console.log("token =>",token);
-  console.log("tokenSecret =>",tokenSecret);
-  cb();
-}
-));
+passport.use(
+  new TwitterStrategy(
+    {
+      consumerKey: TWITTER_CONSUMER_KEY,
+      consumerSecret: TWITTER_CONSUMER_SECRET,
+      callbackURL: "/auth/twitter/callback",
+      proxy: true
+    },
+    async (token, tokenSecret, profile, done) => {
+      try {
+        //before we do create a new user, we need to be sure if that user does not exist in our database;
+        const existingUser = await User.findOne({ twitterId: profile.id });
+        //if no exisiting user,
+        if (existingUser) {
+          return done(null, existingUser);
+        }
+        const user = new User({ twitterId: profile.id });
+        await user.save();
+        return done(null, user);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  )
+);
+
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+})
