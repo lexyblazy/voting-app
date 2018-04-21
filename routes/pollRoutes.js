a60126773f4ceaddb2afa4e3532336661d98ee57
lexy <lexyblazy@gmail.com>
2018-04-21 17:20:54 +0100

    eslint setup

--- routes/pollRoutes.js
+++ routes/pollRoutes.js
@@ -1,5 +1,7 @@
-const router = require('express').Router({mergeParams: true});
+const router = require("express").Router({ mergeParams: true });
 
-router.get('/polls',(req,res)=>{
+router.get("/polls", (req, res) => {
   res.send(req.user);
-})
+});
+
+module.exports = router;
