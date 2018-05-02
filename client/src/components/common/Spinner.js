95a4513b50b8788435c3096ed1932e1f99b1a1b7
lexy <lexyblazy@gmail.com>
2018-05-02 14:37:09 +0100

    created commons folder for reusable components

---
+++ client/src/components/common/Spinner.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { baseColor } from "./colors";
+
+const Spinner = ({size}) => {
+  return (
+    <div className={`preloader-wrapper ${size} active`}>
+      <div style={{ backgroundColor: baseColor }} className="spinner-layer spinner-yellow-only">
+        <div className="circle-clipper left">
+          <div className="circle" />
+        </div>
+        <div className="gap-patch">
+          <div className="circle" />
+        </div>
+        <div className="circle-clipper right">
+          <div className="circle" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { Spinner };
---
+++ client/src/components/common/colors.js
@@ -0,0 +1,1 @@
+export const baseColor = "#e67e22";
